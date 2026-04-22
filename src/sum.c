e46798b36885cec5aa141a37a64562244cc35c3d
drsharapov_24 <daniilsharapow@yandex.ru>
2026-04-22 21:26:27 +0300

    fix: redefenition Ogita-Oishi algorithm via TwoSum by Knuth

--- src/sum.c
+++ src/sum.c
@@ -32,19 +32,13 @@ double sum_kahan(const double* values, size_t n)
     return sum;
 }
 
-/*
-Разложение суммы (`a` + `b`) = (`s` + `e`), где `s` - результат с округлением,
-а `e` - потерянная часть суммы
-*/
-static void two_sum(double a, double b, double* s, double* e)
+/** @brief Error-free transformation двух чисел с плавающей точкой. */
+static void two_sum_knuth(double a, double b, double* x, double* y)
 {
-    *s = a + b;
-    
-    if (fabs(a) >= fabs(b)) {
-        *e = (a - *s) + b;
-    } else {
-        *e = (b - *s) + a;
-    }
+    *x = a + b;
+
+    double z = *x - a;
+    *y = (a - (*x - z)) + (b - z);
 }
 
 double sum_kbn2(const double* values, size_t n)
@@ -55,12 +49,12 @@ double sum_kbn2(const double* values, size_t n)
 
     for (size_t i = 0; i < n; i++) {
         double temp, delta0;
-        two_sum(sum, values[i], &temp, &delta0);
+        two_sum_knuth(sum, values[i], &temp, &delta0);
 
         sum = temp;
 
         double new_err1, delta1;
-        two_sum(err1, delta0, &new_err1, &delta1);
+        two_sum_knuth(err1, delta0, &new_err1, &delta1);
 
         err1 = new_err1;
         err2 += delta1;
@@ -80,17 +74,17 @@ double sum_kbn3(const double* values, size_t n)
 
     for (size_t i = 0; i < n; i++) {
         double temp, delta0;
-        two_sum(sum, values[i], &temp, &delta0);
+        two_sum_knuth(sum, values[i], &temp, &delta0);
 
         sum = temp;
 
         double new_err1, delta1;
-        two_sum(err1, delta0, &new_err1, &delta1);
+        two_sum_knuth(err1, delta0, &new_err1, &delta1);
         
         err1 = new_err1;
         
         double new_err2, delta2;
-        two_sum(err2, delta1, &new_err2, &delta2);
+        two_sum_knuth(err2, delta1, &new_err2, &delta2);
 
         err2 = new_err2;
         err3 += delta2;
@@ -103,18 +97,21 @@ double sum_kbn3(const double* values, size_t n)
 
 double sum_ogita_oishi(const double* values, size_t n)
 {
-    double sum = 0.0;
-    double err = 0.0;
+    if (n == 0) return 0.0;
 
-    for (size_t i = 0; i < n; i++) {
-        double temp  = sum + values[i];
-        double delta = (sum - temp) + values[i];
+    double pi_acc = values[0];
+    double sigma  = 0.0;
 
-        sum = temp;
-        err += delta;
+    for (size_t i = 1; i < n; i++) {
+        double q;
+
+        two_sum_knuth(pi_acc, values[i], &pi_acc, &q);
+        sigma += q;
     }
 
-    return sum + err;
+    double total = pi_acc + sigma;
+
+    return total;
 }
 
 double sum_gmp(const double* values, size_t n)
